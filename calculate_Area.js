78749ec152ec36d020ea7143b2eea7eec191d50e
efflorense <efflorense@icloud.com>
2025-09-02 15:21:30 -0400

    message

---
+++ calculate_Area.js
@@ -0,0 +1,7 @@
+let length, width;
+function calculateArea(){
+    length = parseFloat(document.getElementById('length').value);
+    width = parseFloat(document.getElementById('width').value);
+    let area = width * length;
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}.`
+}
