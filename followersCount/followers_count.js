1d6dc6f8190671eba7a71b2806cb4c30cefcaf4d
efflorense <efflorense@icloud.com>
2025-09-03 09:27:51 -0400

    followersCount

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -4,13 +4,24 @@ function increaseCount(){
     displayCount();
     checkCountValue();
 }
+function decreaseCount(){
+    count = 0;
+    displayCount();
+    checkCountValue();
+}
+
 function displayCount() {
     document.getElementById('countDisplay').innerHTML=count;
 }
 function checkCountValue() {
-    if (count === 10) {
-      alert("Ваш пост в Instagram отримав 10 підписників! Вітаємо!");
+    if (count === 0){
+        alert("Лічильник підписників було скинуто");
     } else if (count === 20) {
       alert("Ваш пост в Instagram отримав 20 підписників! Продовжуйте в тому ж дусі!");
     }
+    else if (count === 10) {
+        alert("Ваш пост в Instagram отримав 10 підписників! Вітаємо!");
+    
+    }
   }
+
